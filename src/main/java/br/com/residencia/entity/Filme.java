a6b956b26ec3088f4ea0b1597fefad36ae0bffb2
Yuri <yurimattos96@gmail.com>
2022-10-13 16:42:52 -0300

    Adicionando a entidade Filme

--- src/main/java/br/com/residencia/entity/Filme.java
+++ src/main/java/br/com/residencia/entity/Filme.java
@@ -1,8 +1,103 @@
-package br.com.residencia.entity;
+package br.com.residencia.cinema.entity;
 
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idFilme")
 @Entity
+@Table(name="filme")
 public class Filme {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_filme")
+	private Integer idFilme;
+	
+	@Column(name="nome_br")
+	private String nomeBr;
+	
+	@Column(name="nome_en")
+	private String nomeEn;
+	
+	@Column(name="ano_lancamento")
+	private Integer anoLancamento;
+	
+	@Column(name="sinopse")
+	private String sinopse;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_diretor", referencedColumnName = "id_diretor")
+	private Diretor diretor;
+	
+	@ManyToOne
+	@JoinColumn(name = "id_genero", referencedColumnName = "id_genero")
+	private Genero genero;
 
+	public Integer getIdFilme() {
+		return idFilme;
+	}
+
+	public void setIdFilme(Integer idFilme) {
+		this.idFilme = idFilme;
+	}
+
+	public String getNomeBr() {
+		return nomeBr;
+	}
+
+	public void setNomeBr(String nome_br) {
+		this.nomeBr = nome_br;
+	}
+
+	public String getNomeEn() {
+		return nomeEn;
+	}
+
+	public void setNomeEn(String nome_en) {
+		this.nomeEn = nome_en;
+	}
+
+	public Integer getAnoLancamento() {
+		return anoLancamento;
+	}
+
+	public void setAnoLancamento(Integer ano_lancamento) {
+		this.anoLancamento = ano_lancamento;
+	}
+
+	public String getSinopse() {
+		return sinopse;
+	}
+
+	public void setSinopse(String sinopse) {
+		this.sinopse = sinopse;
+	}
+
+	public Diretor getDiretor() {
+		return diretor;
+	}
+
+	public void setDiretor(Diretor diretor) {
+		this.diretor = diretor;
+	}
+
+	public Genero getGenero() {
+		return genero;
+	}
+
+	public void setGenero(Genero genero) {
+		this.genero = genero;
+	}
+	
+	
+	
 }
