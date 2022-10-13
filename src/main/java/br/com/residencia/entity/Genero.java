0b213f7292546c90ae1948c48ac007cb0fa2480a
Rafael "Fesaro" Rodrigues <111184544+Rafaelfesaro@users.noreply.github.com>
2022-10-13 17:14:38 -0300

    Segue a entidade Genero

---
+++ src/main/java/br/com/residencia/entity/Genero.java
@@ -0,0 +1,56 @@
+package br.com.residencia.cinema.entity;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idGenero")
+@Entity
+@Table(name="genero")
+public class Genero {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_genero")
+	private Integer idGenero;
+	
+	@Column(name="descricao")
+	private String descricao;
+	
+	//@JsonIgnore
+	@OneToMany(mappedBy = "genero")
+	private Set<Filme> filme;
+
+	public Integer getIdGenero() {
+		return idGenero;
+	}
+
+	public void setIdGenero(Integer idGenero) {
+		this.idGenero = idGenero;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Set<Filme> getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Set<Filme> filme) {
+		this.filme = filme;
+	}
+	
+}
