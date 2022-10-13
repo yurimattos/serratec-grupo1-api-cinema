c629f8559d3810211b2b6da3effdcff77671452e
bruno janiques <brunobrumjaniques@gamil.com>
2022-10-13 16:41:35 -0300

    commit Diretor entity

---
+++ src/main/java/br/com/residencia/entity/Diretor.java
@@ -0,0 +1,59 @@
+package br.com.residencia.cinema.entity;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "idDiretor")
+@Entity
+@Table(name="diretor")
+public class Diretor {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id_diretor")
+	private Integer idDiretor;
+	
+	@Column(name="nome")
+	private String nome;
+	
+	//@JsonIgnore
+	@OneToMany(mappedBy = "diretor")
+	private Set<Filme> filme;
+
+	public Integer getIdDiretor() {
+		return idDiretor;
+	}
+
+	public void setIdDiretor(Integer idDiretor) {
+		this.idDiretor = idDiretor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Set<Filme> getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Set<Filme> filme) {
+		this.filme = filme;
+	}
+
+	
+	
+	
+}
